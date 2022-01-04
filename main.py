a41e2a4f8acfe5fa4dcadf20b77728598fedc433
Seth <sethmb@protonmail.com>
2022-01-04 16:10:24 +0000

    Fix dialog

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit
+#UI
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QLabel, QDialog
 from PyQt5 import uic
+from PyQt5.uic import loadUi
+
+#Functionality
+import os
 import sys
+import subprocess
+
+class SystemInformationDialog(QDialog):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        uic.loadUi("dialogs/systeminfo.ui", self)
+
+        self.quitButton = self.findChild(QPushButton, "quitButton")
+        self.quitButton.clicked.connect(self.QuitButton)
+
+    def QuitButton(self):
+        quit()
+
+
+
 
 
 class UI(QMainWindow):
@@ -9,16 +29,39 @@ class UI(QMainWindow):
         uic.loadUi("helper.ui", self)
 
         # Configure quit quitButton
-        self.quitbutton = self.findChild(QPushButton, "quitButton")
-        self.quitbutton.clicked.connect(self.QuitClicked)
+        self.quitButton = self.findChild(QPushButton, "quitButton")
+        self.quitButton.clicked.connect(self.QuitClicked)
 
         # Get status label
-        self.statuslabel = self.findChild(QLabel, "statusLabel")
+        self.statusLabel = self.findChild(QLabel, "statusLabel")
+
+        #System tab
+
+        # Clear cache Button
+        self.clearCacheButton = self.findChild(QPushButton, "clearCacheButton")
+        self.clearCacheButton.clicked.connect(self.ClearCache)
+
+        # System Information Button
+        self.systemInfoButton = self.findChild(QPushButton, "systemInfoButton")
+        self.systemInfoButton.clicked.connect(self.SystemInformation)
 
         self.show()
 
     def QuitClicked(self):
         exit()
+
+    def ClearCache(self):
+        self.statusLabel.setText("Awaiting authentication...") # This does not seem to work
+        status = os.system("sudo sysctl vm.drop_caches=3")
+        if status == 0:
+            self.statusLabel.setText("Cache cleared!")
+        else:
+            self.statusLabel.setText("Error:", str(status))
+
+    def SystemInformation(self):
+        dialog = SystemInformationDialog()
+        dialog.exec()
+
 
 app = QApplication(sys.argv)
 window = UI()
